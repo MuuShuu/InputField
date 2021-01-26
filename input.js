f2ae89418cdd1789d3c3bfa864abc386d9243418
MuuShuu <sasha.osipun1994@gmail.com>
2021-01-27 00:16:51 +0200

    1

--- input.js
+++ input.js
@@ -8,3 +8,11 @@ btn.addEventListener('click', function() {
     document.body.appendChild(newElement)
     inputField.value = ''
 });
+
+const select = document.getElementById('select')
+
+select.addEventListener('change', function(){
+    const selectColor = select.options[select.selectedIndex].value
+    const newElement = document.getElementById('colorNow');
+    newElement.innerHTML = selectColor;
+});
